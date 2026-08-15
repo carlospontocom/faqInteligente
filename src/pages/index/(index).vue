e2d26f7b2131f5d6b6c13a4aec0c23c11ebfee18
unknown <frontendcnn@gmail.com>
2026-08-14 22:59:59 -0300

    Mudando o login para formulario contato - login último (closes #2)

--- src/pages/index/(index).vue
+++ src/pages/index/(index).vue
@@ -1,23 +1,62 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="column items-center">
-      <img
-        alt="Quasar logo"
-        src="~@/assets/quasar-logo-vertical.svg"
-        style="width: 200px; height: 200px"
-      />
+  <div class="form-header">
+    <h1>Formulário de contato</h1>
+    <p>Para saber mais faça contato conosco</p>
+  </div>
 
-      <q-btn
-        class="q-mt-md"
-        color="primary"
-        to="/second"
-        label="Go to Second Page"
-        no-caps
-      />
+  <q-form class="form" @submit="validarMensagem" greedy>
+    <q-input
+      label="Nome"
+      v-model="nome"
+      :rules="[(val) => !!val || 'Informe seu nome']"
+    />
+    <q-input
+      label="Email"
+      v-model="email"
+      :rules="[
+        (val) => !!val || 'Informe seu e-mail',
+        (val) => /.+@.+\..+/.test(val) || 'E-mail inválido',
+      ]"
+    />
+    <q-input
+      label="Assunto"
+      v-model="assunto"
+      :rules="[(val) => !!val || 'Informe o assunto']"
+    />
+    <q-input
+      label="Mensagem"
+      type="textarea"
+      class=""
+      v-model="mensagem"
+      :rules="[(val) => !!val || 'Digite a mensagem']"
+    />
+
+    <div class="flex q-gutter-md">
+      <q-btn label="Enviar" color="primary" type="submit" />
+      <q-btn label="Cancelar" color="red" type="reset" />
     </div>
-  </q-page>
+  </q-form>
 </template>
 
-<script setup lang="ts">
-//
+<script setup>
+import "../../css/formLogin.scss";
+import { ref } from "vue";
+const nome = ref("");
+const email = ref("");
+const assunto = ref("");
+const mensagem = ref("");
+
+const validarMensagem = () => {
+  console.log({
+    Nome: nome.value,
+    email: email.value,
+    assunto: assunto.value,
+    mensagem: mensagem.value,
+  });
+
+  nome.value = "";
+  email.value = "";
+  assunto.value = "";
+  mensagem.value = "";
+};
 </script>
